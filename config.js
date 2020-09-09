21bd1c92cc5470f06bbb1ef2b00ec5c52cc88d3c
Donghoon Song <thdehdgns@gmail.com>
2020-09-09 23:01:49 +0900

    [change] meta subtitle

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   url: 'https://donghoon759.github.io',
   pathPrefix: '/',
   title: '사진찍는 웹 개발자',
-  subtitle: 'Vue.js를 이용해 FrontEnd 개발을 하고 있습니다.',
+  subtitle: '사람들의 꿈을 이어주는 코멘토에서 일하고 있습니다. Vue.js를 이용해 FrontEnd 개발을 하고 있습니다.',
   copyright: '© All rights reserved.',
   disqusShortname: 'Donghoon',
   postsPerPage: 4,
